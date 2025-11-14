b0e771198277015c9a51bf542b07a1889ea774cd
joelarula <joel.arula@gmail.com>
2025-11-15 00:46:38 +0200

    test

--- tests/bluedevicetest.rs
+++ tests/bluedevicetest.rs
@@ -71,17 +71,24 @@ async fn test_laser_device_functionality(device: &mut BlueLaserDevice) -> Result
 
     //test_shapes(device).await;
 
-    sleep(Duration::from_millis(500));
+    //sleep(Duration::from_millis(500));
     
-//    test_show_drawings(device).await;
-    
+    //test_show_text(device).await;
 
     sleep(Duration::from_millis(500));
     
-    test_show_text(device).await;
+    test_show_drawings(device).await;
 
+    sleep(Duration::from_millis(500));
+    
+    test_show_drawing_protocol(device).await;
 
     Ok(())
+}
+
+
+async fn test_show_drawing_protocol(device: &mut BlueLaserDevice) {
+
 }
 
 async fn test_show_text(device: &mut BlueLaserDevice) { 
